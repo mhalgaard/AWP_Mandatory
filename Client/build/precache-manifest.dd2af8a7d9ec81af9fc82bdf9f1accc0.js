adaaca412811ad2f9b6b0dba672a2cbc57273eda
Mikkel Halgaard Kjær <mhalgaard94@gmail.com>
2020-04-20 13:03:14 +0200

    newupdate

---
+++ Client/build/precache-manifest.dd2af8a7d9ec81af9fc82bdf9f1accc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea04c40d24c35f3a86a4f30aa4cf1a98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e0baf703b6985d48ca1",
+    "url": "/static/css/main.9e2fa8c0.chunk.css"
+  },
+  {
+    "revision": "45ab5e8c4bd81a1dc197",
+    "url": "/static/js/2.d86bd938.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.d86bd938.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e0baf703b6985d48ca1",
+    "url": "/static/js/main.510931f4.chunk.js"
+  },
+  {
+    "revision": "94578fdd3162bbf884ee",
+    "url": "/static/js/runtime-main.583572ab.js"
+  }
+]);
